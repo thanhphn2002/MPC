0d3f7f25ff6f7f01dcf809bd2ee9aed7c8d01231
thanhphn2002 <phnthanh2002@gmail.com>
2022-03-20 15:44:54 +0700

    MPC

---
+++ JS/login.js
@@ -0,0 +1,13 @@
+function changeToSignIn(){
+    var overlay1 = document.getElementById("overlay1");
+    var overlay2 = document.getElementById("overlay2");
+    overlay2.style.display = "none";
+    overlay1.style.display = "block";
+}
+
+function changeToSignUp(){
+    var overlay1 = document.getElementById("overlay1");
+    var overlay2 = document.getElementById("overlay2");
+    overlay1.style.display = "none";
+    overlay2.style.display = "block";
+}
